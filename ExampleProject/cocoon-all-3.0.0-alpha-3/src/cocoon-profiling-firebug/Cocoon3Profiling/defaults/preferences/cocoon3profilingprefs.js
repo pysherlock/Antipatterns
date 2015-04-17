232fe2e7551086caa5f7a1e38282114e059a2b7b
PuYang <pu_yang@outlook.com>
2015-04-17 20:30:32 +0800

    Transform to Windows_Begin with xalan

--- ExampleProject/cocoon-all-3.0.0-alpha-3/src/cocoon-profiling-firebug/Cocoon3Profiling/defaults/preferences/cocoon3profilingprefs.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.firebug.cocoon3profiling.sitemapenabled", true);
-pref("extensions.firebug.cocoon3profiling.openinvocations", true);
-pref("extensions.firebug.cocoon3profiling.showmatcher", true);
